2a2c189578e120bcd9d47524cfbce394acb4f98a
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-02-27 21:08:36 +0300

    Models added.

--- FormValidationWithCombineFramework/Core/Model.swift
+++ FormValidationWithCombineFramework/Core/Model.swift
@@ -8,25 +8,7 @@
 import Foundation
 import UIKit
 
-protocol FormItem{
-    var id: UUID {get}
-    var formId: FormField {get}
-    var validations: [ValidationManager] {get}
-}
 
-protocol FormSectionItem{
-    var id : UUID {get}
-    var items: [FormComponent] {get}
-    init(items: [FormComponent])
-}
-
-enum FormField: String,CaseIterable{
-    case firstName
-    case lastName
-    case email
-    case dob
-    case submit
-}
 
 final class FormSectionComponent : FormSectionItem, Hashable{
     let id: UUID = UUID()
